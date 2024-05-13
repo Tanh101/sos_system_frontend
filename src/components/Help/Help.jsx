d9136d21a787e4beb4e91007838587c8e916a671
Tanh101 <lyvantanh101@gmail.com>
2024-05-13 21:07:04 +0700

    create post ui and connect to api

--- src/components/Help/Help.jsx
+++ src/components/Help/Help.jsx
@@ -1,17 +1,32 @@
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 
+import Post from "./Post/Post";
 import { UserContext } from "../../Context/UserContext/UserContext";
+import RequestService from "../../services/RequestService";
+import Loading from "../Loading/Loading";
 
 const Help = () => {
+    const { getRequests } = RequestService();
     const { setActiveItem } = useContext(UserContext);
+
+    const [requestData, setRequestData] = useState({});
 
     useEffect(() => {
         setActiveItem('help');
+        const fetchRequesets = async () => {
+            const requests = await getRequests();
+            setRequestData(requests);
+        }
+        fetchRequesets();
     }, []);
 
+    console.log(requestData);
+
     return (
-        <div className="flex flex-1 w-full h-screen bg-white justify-center items-center">
-            Help
+        <div className="flex flex-1 h-screen bg-white  justify-center items-start overflow-y-auto py-5">
+            <div className="flex flex-col h-screen xl:mx-72 lg:mx-20 md:mx-16 mx-10 routded-lg pb-20">
+                <Post requestData={requestData} />
+            </div>
         </div>
     )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
-import './index.css'
-import ReactDOM from 'react-dom/client'
+import "./index.css";
+import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { ToastContainer } from 'react-toastify'
+import { ToastContainer } from "react-toastify";
 
-import Login from './pages/Login/Login.jsx';
-import Signup from './pages/Signup/Signup.jsx';
-import UserNavigation from './pages/Navigation/UserNavigate/UserNavigate.jsx';
+import Login from "./pages/Login/Login.jsx";
+import Signup from "./pages/Signup/Signup.jsx";
+import UserNavigation from "./pages/Navigation/UserNavigate/UserNavigate.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
     <Router>
         <Routes>
             <Route path="/login" element={<Login />} />
@@ -16,4 +16,4 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         </Routes>
         <ToastContainer />
     </Router>
-)
+);
